93e758aeae0c643cd8de368e05c112407faa33d9
bhaskarkesavulu <bhaskar120689@gmail.com>
2024-02-11 12:34:47 +0530

    SI

---
+++ src/com/setter/di/Test.java
@@ -0,0 +1,14 @@
+package com.setter.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+    public static void main(String[] args){
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Employee employee = (Employee) context.getBean("obj");
+        employee.display();
+    }
+}
